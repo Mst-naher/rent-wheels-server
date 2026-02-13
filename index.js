624e933defee700fc3f2876f4c8a66bb00b39bdd
mst <mst.naher.bd@gmail.com>
2026-02-13 13:08:15 +0000

    created api's like post, delete, patch, get, and added vercel's json file

--- index.js
+++ index.js
@@ -25,17 +25,67 @@ app.get("/", (req, res) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("rent_db");
     const productCollection = db.collection("products");
     const userCollection = db.collection("users");
-    //  Featured Cars:
-    app.get("/products", async (req, res) => {
-      const result = await productCollection.find().toArray(); //promise resolving here
+    const bookingCollection = db.collection("booking");
 
+    //Booking api:
+    app.post("/booking", async (req, res) => {
+      const newProduct = req.body;
+      // const email = req.body.email;
+      // const query = { created_by:email };
+      const result = await bookingCollection.insertOne(newProduct);
       res.send(result);
     });
+
+    app.get("/booking", async (req, res) => {
+      const cursor = bookingCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedBooking = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: { updatedBooking },
+      };
+      const result = await bookingCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //  Featured Cars:
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find().sort().skip(0).limit(6);
+
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // app.get('/products', async(req, res) => {
+    //   const id= req.params.id;
+    //   const query = {_id: new ObjectId(id)}
+    //   const result = await productCollection.findOne(query)
+    //   res.send(result)
+    // })
 
     app.get("/users", async (req, res) => {
       const cursor = userCollection.find();
@@ -47,6 +97,7 @@ async function run() {
     app.post("/users", async (req, res) => {
       const data = req.body;
       console.log("user information", data);
+
       const result = await userCollection.insertOne(data);
       res.send({
         success: true,
@@ -61,7 +112,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
